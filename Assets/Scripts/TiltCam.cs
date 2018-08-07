f49beda40f2103341f7d2f705d4959734858c04a
= <->
2018-08-07 21:30:13 +0700

    [ remove old path system ]

--- Assets/Scripts/TiltCam.cs
+++ Assets/Scripts/TiltCam.cs
@@ -10,6 +10,12 @@ public class TiltCam : MonoBehaviour {
         float changeX = Input.GetAxis("Mouse X");
         float changeY = Input.GetAxis("Mouse Y");
 
+        if(Input.touchCount > 0)
+        {
+            changeX = Input.touches[0].deltaPosition.x/10f;
+            changeY = Input.touches[0].deltaPosition.y/10f;
+        }
+
         for (int i = 0; i < 4; i++)
         {
             transform.Rotate(-changeY, 0, 0, Space.Self);
