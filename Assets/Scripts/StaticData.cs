8683f007d203b1ba4365a585f0f99d3476f9ec3f
= <->
2018-08-16 00:18:27 +0700

    [ irrelevant change ]

--- Assets/Scripts/StaticData.cs
+++ Assets/Scripts/StaticData.cs
@@ -1,5 +1,30 @@
-﻿public class StaticData {
-    public static int playerId = 0;
-    public static string serverIp = "0.0.0.0";
-    public static string serverPort = "1979";
+﻿using UnityEngine;
+
+public class StaticData {
+    public static int playerId {
+        get{
+            return PlayerPrefs.GetInt("playerId");
+        }
+        set{
+            PlayerPrefs.SetInt("playerId", value);
+        }
+    }
+
+    public static string serverIp {
+        get{
+            return PlayerPrefs.GetString("serverIp");
+        }
+        set{
+            PlayerPrefs.SetString("serverIp", value);
+        }
+    }
+
+    public static string serverPort {
+        get{
+            return PlayerPrefs.GetString("serverPort");
+        }
+        set{
+            PlayerPrefs.SetString("serverPort", value);
+        }
+    }
 }
