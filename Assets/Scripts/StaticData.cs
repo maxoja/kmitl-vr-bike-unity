0504eb6d070cd962b097f9ca93e69d0ca3f48d9d
= <->
2018-09-05 15:01:13 +0700

    [ fixed ip/port for edit mode ]

--- Assets/Scripts/StaticData.cs
+++ Assets/Scripts/StaticData.cs
@@ -1,27 +1,44 @@
 ﻿using UnityEngine;
 
-public class StaticData {
-    public static int playerId {
-        get{
+public class StaticData
+{
+    public static int playerId
+    {
+        get
+        {
             return PlayerPrefs.GetInt("playerId", 0);
         }
-        set{
+        set
+        {
             PlayerPrefs.SetInt("playerId", value);
         }
     }
 
-    public static string serverIp {
-        get{
+    public static string serverIp
+    {
+        get
+        {
+#if UNITY_EDITOR
+            return "localhost";
+#else
             return PlayerPrefs.GetString("serverIp", "192.168.0.3");
+#endif
         }
-        set{
+        set
+        {
             PlayerPrefs.SetString("serverIp", value);
         }
     }
 
-    public static string serverPort {
-        get{
+    public static string serverPort
+    {
+        get
+        {
+#if UNITY_EDITOR
+            return "1996";
+#else
             return PlayerPrefs.GetString("serverPort","1996");
+#endif
         }
         set{
             PlayerPrefs.SetString("serverPort", value);
